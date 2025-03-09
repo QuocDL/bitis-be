eb98b2ff4d3585788623cce89410ab07c8b240be
Kimntm24 <nguyenthimykim659@gmail.com>
2025-03-09 11:46:37 +0700

    feat: get details and update color service

--- src/services/color.service.js
+++ src/services/color.service.js
@@ -10,3 +10,13 @@ export const createColor = asyncHandler(async (req, res, next) => {
 export const getAllColors = asyncHandler(async (req, res, next) => {
     return colorServices.getAllColors(req, res, next);
 });
+
+// @Get get detailed color
+export const getDetailedColor = asyncHandler(async (req, res, next) => {
+    return colorServices.getDetailedColor(req, res, next);
+});
+
+// @Patch update color
+export const updateColor = asyncHandler(async (req, res, next) => {
+    return colorServices.updateColor(req, res, next);
+});
