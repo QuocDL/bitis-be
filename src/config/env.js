ad0355b661963b88db24bdcca9583f1926bc1880
Kimntm24 <nguyenthimykim659@gmail.com>
2025-03-11 14:52:06 +0700

    feat: verify email và quên mật khảu

--- src/config/env.js
+++ src/config/env.js
@@ -1,7 +1,12 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 dotenv.config();
 
 export const envConfig = {
-    JWT_SECRET:process.env.JWT_SECRET,
-    NODE_ENV:process.env.NODE_ENV
-}
+    JWT_SECRET: process.env.JWT_SECRET,
+    JWT_VERIFY: process.env.JWT_VERIFY,
+    NODE_ENV: process.env.NODE_ENV,
+    nodeMailer: {
+        email: process.env.EMAIL_USER,
+        password: process.env.EMAIL_PASSWORD,
+    },
+};
