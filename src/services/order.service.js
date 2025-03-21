cb150aefca825a3b6892e4f46f199e8f541a8aed
thuynh2302 <thuynhph45833@fpt.edu.vn>
2025-03-21 11:19:15 +0700

    fix: validate và kiểm tra stock trước khi tạo order

--- src/services/order.service.js
+++ src/services/order.service.js
@@ -116,3 +116,84 @@ export const createOrder = async (req, res, next) => {
         }),
     );
 };
+
+//@POST Set order status to cancelled
+export const cancelOrder = async (req, res, next) => {
+    const foundedOrder = await Order.findOne({ _id: req.body.orderId });
+
+    if (!foundedOrder) {
+        throw new BadRequestError(`Not found order with id ${req.body.orderId}`);
+    }
+
+    if (foundedOrder.orderStatus === ORDER_STATUS.CANCELLED) {
+        throw new NotAcceptableError(`You cannot cancel this order because it was cancelled before. `);
+    }
+
+    if (foundedOrder.orderStatus !== ORDER_STATUS.DELIVERED && foundedOrder.orderStatus !== ORDER_STATUS.DONE) {
+        if (req.role !== ROLE.ADMIN && foundedOrder.orderStatus !== ORDER_STATUS.PENDING) {
+            throw new NotAcceptableError('Bạn không được phép hủy đơn vui lòng liên hệ nếu có vấn đề');
+        }
+        if (req.role === ROLE.ADMIN) {
+            foundedOrder.canceledBy = ROLE.ADMIN;
+        }
+
+        foundedOrder.orderStatus = ORDER_STATUS.CANCELLED;
+        foundedOrder.description = req.body.description ?? '';
+        foundedOrder.save();
+
+        // Update stock
+        await inventoryService.updateStockOnCancelOrder(foundedOrder.items);
+
+        const template = {
+            content: {
+                title: `${req.role === ROLE.ADMIN ? 'Đơn hàng của bạn đã bị hủy bởi admin' : 'Đơn hàng của bạn đã bị hủy'}`,
+                description: `${req.role === ROLE.ADMIN ? `Đơn hàng của bạn đã bị hủy bởi admin với lý do ${foundedOrder.description}, ${foundedOrder.isPaid ? `Rất xin lỗi vì sự bất tiện này hãy liên hệ ngay với chúng tôi qua số điện thoại +84 123 456 789 để cửa hàng hoàn lại ${new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(foundedOrder.totalPrice || 0)} cho bạn ` : ''} dưới đây là thông tin đơn hàng:` : `Bạn vừa hủy một đơn hàng với lý do ${foundedOrder.description} từ AdShop thông tin đơn hàng:`}`,
+                email:
+                    foundedOrder.paymentMethod === PAYMENT_METHOD.CARD
+                        ? foundedOrder.customerInfo.email
+                        : foundedOrder.receiverInfo.email,
+            },
+            product: {
+                items: foundedOrder.items,
+                shippingfee: foundedOrder.shippingFee,
+                totalPrice: foundedOrder.totalPrice,
+            },
+            subject: '[AdShop] - Đơn hàng của bạn đã bị hủy',
+            link: {
+                linkHerf: `http://localhost:3000/my-orders/${req.body.orderId}`,
+                linkName: `Kiểm tra đơn hàng`,
+            },
+            user: {
+                name:
+                    foundedOrder.paymentMethod === PAYMENT_METHOD.CARD
+                        ? foundedOrder.customerInfo.name
+                        : foundedOrder.receiverInfo.name,
+                phone:
+                    foundedOrder.paymentMethod === PAYMENT_METHOD.CARD
+                        ? foundedOrder.customerInfo.phone
+                        : foundedOrder.receiverInfo.phone,
+                email:
+                    foundedOrder.paymentMethod === PAYMENT_METHOD.CARD
+                        ? foundedOrder.customerInfo.email
+                        : foundedOrder.receiverInfo.email,
+                address: `[${foundedOrder.shippingAddress.address}] -${foundedOrder.paymentMethod === PAYMENT_METHOD.CARD ? '' : ` ${foundedOrder.shippingAddress.ward}, ${foundedOrder.shippingAddress.district},`} ${foundedOrder.shippingAddress.province}, ${foundedOrder.shippingAddress.country}`,
+            },
+        };
+        await sendMail({
+            email: foundedOrder.customerInfo.email,
+            template,
+            type: 'UpdateStatusOrder',
+        });
+    } else {
+        throw new NotAcceptableError(`Đơn hàng của bạn đã được giao không thể hủy đơn`);
+    }
+
+    return res.status(StatusCodes.OK).json(
+        customResponse({
+            data: null,
+            success: true,
+            status: StatusCodes.OK,
+            message: 'Your order is cancelled.',
+        }),
+    );
+};
