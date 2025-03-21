d9fb9fd67e0b764b59486b4024c8ae594472963d
thangmanh111 <thangnmt047@gmail.com>
2025-03-21 11:12:44 +0700

    feat: viết hàm kiểm tra stock của sản phẩm xem còn hàng không

---
+++ src/services/inventory.service.js
@@ -0,0 +1,28 @@
+import { BadRequestError, NotFoundError } from '../errors/customError.js';
+import Product from '../models/product.js';
+
+export const updateStockOnCreateOrder = async (dataItems, session) => {
+    return await Promise.all(
+        dataItems.map(async (item) => {
+            const productTarget = await Product.findOne({
+                _id: item.productId,
+            }).session(session);
+            if (!productTarget) {
+                throw new NotFoundError('Product not found');
+            }
+            const newVariants = productTarget.variants.map((variant) => {
+                if (variant._id.toString() === item.variantId.toString()) {
+                    const newStock = variant.stock - item.quantity;
+                    if (newStock < 0) {
+                        throw new BadRequestError('Sản phẩm đã hết hàng!');
+                    }
+                    variant.stock = newStock;
+                }
+                return variant;
+            });
+            productTarget.sold += item.quantity;
+            productTarget.variants = newVariants;
+            await productTarget.save();
+        }),
+    );
+};
