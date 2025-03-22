72462c37cf74d9dbc6a9369b0fe1409a674ad216
vanhdeptraivcl <vietcongtu66@gmail.com>
2025-03-22 16:17:31 +0700

    feat: xong luồng thanh toán với vnpay

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,22 +1,22 @@
 import asyncHandler from '../helpers/asyncHandler.js';
-import { authService } from '../services/index.js';
+import { authServices } from '../services/index.js';
 
 // @Register
 export const register = asyncHandler(async (req, res, next) => {
-    return await authService.register(req, res, next);
+    return await authServices.register(req, res, next);
 });
 // @Login
 export const login = asyncHandler(async (req, res, next) => {
-    return await authService.login(req, res, next);
+    return await authServices.login(req, res, next);
 });
 // @send mail Verify
 export const sendMailVerify = asyncHandler(async (req, res, next) => {
-    return await authService.sendMailverifyAccount(req, res, next);
+    return await authServices.sendMailverifyAccount(req, res, next);
 });
 export const sendMailResetPassword = asyncHandler(async (req, res, next) => {
-    return await authService.sendMailForgotPassword(req, res, next);
+    return await authServices.sendMailForgotPassword(req, res, next);
 });
 // @Verify
 export const verifyEmail = asyncHandler(async (req, res, next) => {
-    return await authService.verifyEmail(req, res, next);
+    return await authServices.verifyEmail(req, res, next);
 });
---
+++ src/routers/checkout.routes.js
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import { checkoutControllers } from '../controllers/index.js';
+import { transactionMiddleware } from '../middleware/transaction.js';
+
+const checkoutRouter = Router();
+
+checkoutRouter.post(
+    '/create-checkout-with-vnpay',
+    transactionMiddleware,
+    checkoutControllers.createPaymentUrlWithVNpay,
+);
+checkoutRouter.get('/vnpay-return', checkoutControllers.vnpayReturn);
+checkoutRouter.get('/vnpay-ipn', checkoutControllers.vnpayIPN);
+
+export default checkoutRouter;
