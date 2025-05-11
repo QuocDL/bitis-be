6c0b810b2054ab71334f42f3c5b52cc48d9fd4ec
thuynh2302 <nguyenthimykim659@gmail.com>
2025-05-11 13:11:50 +0700

    refactor: chỉnh lại chút code của quản lí lích cỡ

--- src/services/size.service.js
+++ src/services/size.service.js
@@ -3,65 +3,105 @@ import customResponse from '../helpers/response.js';
 import Size from '../models/size.js';
 import handleQuery from '../utils/handleQuery.js';
 
-// @Post create new size
-export const createNewSize = async (req, res, next) => {
-    const size = await Size.create(req.body);
-
-    return res.status(StatusCodes.CREATED).json(
+// Helper function to send responses
+const sendResponse = (res, statusCode, data, message = ReasonPhrases.OK) => {
+    return res.status(statusCode).json(
         customResponse({
-            data: size,
-            message: ReasonPhrases.CREATED,
-            status: StatusCodes.OK,
+            data,
+            message,
+            status: statusCode,
             success: true,
-        }),
+        })
     );
+};
+
+// @Post create new size
+export const createNewSize = async (req, res) => {
+    try {
+        const size = await Size.create(req.body);
+        return sendResponse(res, StatusCodes.CREATED, size, ReasonPhrases.CREATED);
+    } catch (error) {
+        return res.status(StatusCodes.BAD_REQUEST).json(
+            customResponse({
+                message: error.message,
+                status: StatusCodes.BAD_REQUEST,
+                success: false,
+            })
+        );
+    }
 };
 
 // @Get get all size
-export const getAllSizes = async (req, res, next) => {
-    const { data, page, totalDocs, totalPages } = await handleQuery(req, Size);
-
-    return res.status(StatusCodes.OK).json(
-        customResponse({
-            data: {
-                sizes: data,
-                page,
-                totalDocs,
-                totalPages,
-            },
-            message: ReasonPhrases.OK,
-            status: StatusCodes.OK,
-            success: true,
-        }),
-    );
+export const getAllSizes = async (req, res) => {
+    try {
+        const { data, page, totalDocs, totalPages } = await handleQuery(req, Size);
+        return sendResponse(res, StatusCodes.OK, {
+            sizes: data,
+            page,
+            totalDocs,
+            totalPages,
+        });
+    } catch (error) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(
+            customResponse({
+                message: error.message,
+                status: StatusCodes.INTERNAL_SERVER_ERROR,
+                success: false,
+            })
+        );
+    }
 };
 
 // @Get get detailed size
-export const getDetailedSize = async (req, res, next) => {
-    const size = await Size.findById(req.params.id).lean();
-
-    return res.status(StatusCodes.OK).json(
-        customResponse({
-            data: size,
-            message: ReasonPhrases.OK,
-            status: StatusCodes.OK,
-            success: true,
-        }),
-    );
+export const getDetailedSize = async (req, res) => {
+    try {
+        const size = await Size.findById(req.params.id).lean();
+        if (!size) {
+            return res.status(StatusCodes.NOT_FOUND).json(
+                customResponse({
+                    message: 'Size not found',
+                    status: StatusCodes.NOT_FOUND,
+                    success: false,
+                })
+            );
+        }
+        return sendResponse(res, StatusCodes.OK, size);
+    } catch (error) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(
+            customResponse({
+                message: error.message,
+                status: StatusCodes.INTERNAL_SERVER_ERROR,
+                success: false,
+            })
+        );
+    }
 };
 
-// @Post update color
-export const updateSize = async (req, res, next) => {
-    const newSize = await Size.findOneAndUpdate({ _id: req.params.id }, req.body, {
-        new: true,
-    });
+// @Post update size
+export const updateSize = async (req, res) => {
+    try {
+        const newSize = await Size.findOneAndUpdate({ _id: req.params.id }, req.body, {
+            new: true,
+        });
 
-    return res.status(StatusCodes.OK).json(
-        customResponse({
-            data: newSize,
-            message: ReasonPhrases.OK,
-            status: StatusCodes.OK,
-            success: true,
-        }),
-    );
+        if (!newSize) {
+            return res.status(StatusCodes.NOT_FOUND).json(
+                customResponse({
+                    message: 'Size not found',
+                    status: StatusCodes.NOT_FOUND,
+                    success: false,
+                })
+            );
+        }
+
+        return sendResponse(res, StatusCodes.OK, newSize);
+    } catch (error) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(
+            customResponse({
+                message: error.message,
+                status: StatusCodes.INTERNAL_SERVER_ERROR,
+                success: false,
+            })
+        );
+    }
 };
