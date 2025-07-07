fc6ff21e5adbd136cd094bd872974338577a0e20
Kimntm24 <nguyenthimykim659@gmail.com>
2025-07-08 01:47:29 +0700

    update

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { initializeApp } from 'firebase/app';
 import { handleInsertData } from './data/index.js';
 const app = express();
 const corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: 'https://bittis.tuananhlab.com/',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
