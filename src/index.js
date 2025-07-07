d793d49f3e3329ccbc6fa9c6efa2fd5ed9ea05c9
Tuan Anh <anhntph30476@fpt.edu.vn>
2025-07-08 02:35:20 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,12 +9,16 @@ import { envConfig } from './config/env.js';
 import { initializeApp } from 'firebase/app';
 import { handleInsertData } from './data/index.js';
 const app = express();
-const corsOptions = {
-    origin: 'https://bittis.tuananhlab.com/',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+export const corsOptions = {
+    origin: (origin: any, callback: any) => {
+        if (["https://bittis.tuananhlab.com/"].indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true,
-    exposedHeaders: ['Content-Range', 'X-Content-Range', 'Content-Length'],
+    optionsSuccessStatus: 200,
 };
 
 // firebase
@@ -22,7 +26,7 @@ initializeApp(envConfig.FIREBASE);
 
 // middleware
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+
 app.use(express.json());
 
 app.use(morgan('tiny'));
